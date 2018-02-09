772f271bd7218eb14905a547867dc2708e15b953
nurriol2 <nurriol2@illinois.edu>
2018-02-09 16:34:18 -0600

    Made some progress on exercise 5

--- Exercise 1 - 5.py
+++ Exercise 1 - 5.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-import scipy as sci
+import scipy 
 import math
 
 
@@ -12,8 +12,8 @@ def sec_hour_convert(sec):
     sec_in_hour = sec_in_min * min_in_hour
     
     hour = sec/sec_in_hour
-    
-    print('There are ', hour, 'hour(s) in ', sec, 'seconds')
+    #print(hour)
+    #print('There are ', hour, 'hour(s) in ', sec, 'seconds')
     return hour 
 
 
@@ -24,17 +24,23 @@ data = np.loadtxt(fname='/Users/nikourriola/Desktop/Research/Workshop1/MG1655 Ch
 
 
 row1 = data[0,:]
+sec_hour_convert(row1)
 
 
+'''
 print(len(row1))
 #110 elements
 print(row1[109,])
 #110th element is 86221 
 
-time = row1
+'''
 
-sec_hour_convert(time[109,])
+
+'''
+sec_hour_convert(row1[109,])
 #Answer:  There are  23.9502777778 hour(s) in  86221.0 seconds
+'''
+
 
 '''
 #Exercise 2-1
@@ -74,7 +80,7 @@ matrix_3_1 = np.dot(matrix_3_3, vector)
 #Exercise 3 & Exercise 4
 #example_growth_curve = data[1,:]
 
-#plt.plot(time, example_growth_curve)
+#plt.plot(row1, example_growth_curve)
 #plt.xlabel('Time (hours)')
 #plt.ylabel('Absorbance')
 
@@ -84,13 +90,15 @@ matrix_3_1 = np.dot(matrix_3_3, vector)
 for i in range(1,7):
     rows = np.array(data[i,:])
     label_data = ('Row ' + str(i))
-    plt.plot(time,rows, label = label_data)
+    plt.plot(sec_hour_convert(row1),rows, label = label_data)
     
 plt.legend(loc = 'best')
 plt.xlabel('Time(hours)')
-plt.xlabel('Absorbance')
+plt.ylabel('Absorbance')
     
 plt.show()
+
+
 
 
 
@@ -98,7 +106,7 @@ plt.show()
 #One plot for each row of data
 for i in range(1,7):
     rows = np.array(data[i,:])
-    plt.plot(time,rows)
+    plt.plot(row1,rows)
     plt.xlabel('Time(hours)')
     plt.xlabel('Absorbance')
     
@@ -108,7 +116,53 @@ for i in range(1,7):
 
 #Exercise 5
 
-#from sci.optimize import curve_fit
+from scipy.optimize import curve_fit 
+
+#Variables
+#k - carrying capacity
+#c - constant offset 
+#p_0 - initial population size
+#r - growth rate
+#t - time data from row1[0,:]
+#P(t) = ((k*p_0*e^rt)/(k+p_0((e^rt)-1))) + c
+
+def func(time_data):
+    k = 1.4
+    c = 0
+    #p_0 = 0.001
+    p_0 = 0.01
+    r = .25
+    
+    y = np.array([])
+    
+    for t in time_data:
+        x = (k*p_0*math.exp(r*t))/(k+(p_0*(math.exp(r*t) - 1)))
+        y = np.append(y,x)
+    #print(y)
+    plt.plot(sec_hour_convert(row1), y)
+    
+    plt.xlabel('Time(hour(s))')
+    plt.ylabel('Absorbance')
+    
+    plt.show()
+    return y 
+
+func(sec_hour_convert(row1))
+
+curve_fit()
+
+
+
+
+
+
+    
+
+
+
+    
+
+
 
 
 
